47a9d0a853c41815fb6ada9e74bb5f5cd0ca8a6a
Sait Can <saitcan.42@hotmail.com>
2021-01-16 23:46:18 +0300

    16 ocak 2021

---
+++ KampIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // type safety = tip güvenliği
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
+
