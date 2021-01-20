20c7ada29c522dd416d343a026a500b93029e900
Sait Can <saitcan.42@hotmail.com>
2021-01-20 23:51:21 +0300

    20.01.2021 Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Sait", "Ahmet", "Selim", "Neslihan" };
+
+
+            List<string> isimler2 = new List<string> { "Sait", "Ahmet", "Selim", "Neslihan" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Emine");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
